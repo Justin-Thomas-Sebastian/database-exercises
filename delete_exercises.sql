1edb6bfc5725bc3a6d9cf4c55a40a0fa6ab2f085
Justin Sebastian <justinthomassebastian@gmail.com>
2022-03-17 16:55:45 -0600

    finish delete exercises

---
+++ delete_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+-- Albums released after 1991
+DELETE
+FROM albums
+WHERE release_date > 1991;
+
+-- Albums with the genre 'disco'
+DELETE
+FROM albums
+WHERE genre = 'disco';
+
+-- Albums by 'Whitney Houston'
+DELETE
+FROM albums
+WHERE artist = 'Whitney Houston';
