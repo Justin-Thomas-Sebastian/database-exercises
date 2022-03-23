5f4e8911b443878f420b10feee7fb035f3afc29e
Justin Sebastian <justinthomassebastian@gmail.com>
2022-03-23 12:36:23 -0600

    finish bonus and refactor prev questions

--- join_exercises.sql
+++ join_exercises.sql
@@ -29,12 +29,24 @@ WHERE year(titles.to_date) = 9999
 GROUP BY titles.title;
 
 -- Find the current salary of all current managers.
-SELECT DISTINCT departments.dept_name AS Department_Name,
-                CONCAT(employees.first_name, ' ', employees.last_name) AS Department_Manager,
-                salaries.salary
+SELECT departments.dept_name AS Department_Name,
+       CONCAT(employees.first_name, ' ', employees.last_name) AS Department_Manager,
+       salaries.salary AS Salary
 FROM departments
 JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
 JOIN employees ON dept_manager.emp_no = employees.emp_no
 JOIN salaries ON employees.emp_no = salaries.emp_no
 WHERE year(salaries.to_date) = 9999
+    AND year(dept_manager.to_date) = 9999;
+
+-- Find the names of all current employees, their department name, and their current manager's name .
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS Employee,
+       departments.dept_name AS Department,
+       CONCAT(filteredManagers.first_name, ' ', filteredManagers.last_name) AS Manager
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+JOIN employees AS filteredManagers ON dept_manager.emp_no = filteredManagers.emp_no
+WHERE year(dept_emp.to_date) = 9999
     AND year(dept_manager.to_date) = 9999;
