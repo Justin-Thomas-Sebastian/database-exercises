da2109120489c4ce5eb659d280a034f51f44e9a8
Justin Sebastian <justinthomassebastian@gmail.com>
2022-03-24 10:50:53 -0600

    finish subqueries bonus

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -36,3 +36,28 @@ WHERE gender = 'f' AND emp_no IN(
     FROM dept_manager
     WHERE year(to_date) = 9999
 );
+
+-- Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE year(to_date) = 9999 AND emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'f'
+    )
+);
+
+-- Find the first and last name of the employee with the highest salary.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT * FROM (
+        SELECT emp_no
+        FROM salaries
+        ORDER BY salary DESC
+        LIMIT 1
+    ) AS usingADerivedTableBecauseMySQLSucksCauseItDoesntSupportLimitAfterInSubquery
+);
