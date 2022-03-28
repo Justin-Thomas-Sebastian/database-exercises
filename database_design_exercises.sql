39dc0e12a04622bcf56cc6990d6c5d593b8df13a
Justin Sebastian <justinthomassebastian@gmail.com>
2022-03-28 15:19:34 -0600

    create database design practice tables

---
+++ database_design_exercises.sql
@@ -0,0 +1,37 @@
+USE adlister_practice;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    password CHAR(64) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(100),
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE IF NOT EXISTS ad_category (
+    ad_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES advertisements(ad_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+CREATE TABLE IF NOT EXISTS advertisements (
+    ad_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    img VARCHAR(512),
+    create_time DATETIME NOT NULL,
+    update_time DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (category_id) REFERENCES ad_category(category_id),
+    PRIMARY KEY (ad_id)
+);
